e2fa7d111d65b06850added228ba69668c158f32
trieutct <96654274+trieutct@users.noreply.github.com>
2023-11-19 21:28:51 +0700

    ok

---
+++ Clean_Architecture.Api/Controllers/DashBoardController/DashBoardController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Clean_Architecture.Api.Controllers.DashBoardController
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DashBoardController : ControllerBase
+    {
+        
+    }
+}
